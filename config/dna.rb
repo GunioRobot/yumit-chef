dbebbbe05fdbd8d8885098926edb8c8b896d2a26
xurde <jorgedf@gmail.com>
2010-01-25 11:04:05 +0100

    Some templates fixes for yumit.com

--- config/dna.rb
+++ config/dna.rb
@@ -33,7 +33,7 @@ dna = {
   :mysql => {
     :datadir => '/var/lib/mysql',
     :ec2_path => '/db/mysql',
-    :server_root_password => 'belen3',
+    :server_root_password => 'reyes28015',
     :tunable => {
       :max_connections => 100,
       :key_buffer => '40M',
@@ -56,13 +56,13 @@ dna = {
   },
 
   :passenger => {
-    :version => '2.2.4',
+    :version => '2.2.9',
     :max_pool_size => 12,
     :stat_throttle_rate => 10
   },
 
   :rails => {
-    :version => '2.3.2'
+    :version => '2.3.5'
   },
 
   :packages => [
@@ -74,8 +74,7 @@ dna = {
   :users => [{
     :username => 'capistrano',
     :authorized_keys => "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAw5b4okzvmo9TKfqz6zAP4Y38xnBN4UIGfbOO8OrrD9YPOBQiEd8RAvPrZ/oxTSutxABqEtUDowBa36WlRkGX9OVUvwXj+yec5BaBGPk4mqp+7YZJSeKhOrrWKVr43q9uNDxuChJD1+MwME6HnE/RvONPpF8OKEEfkYqd1GmxwGIMhgNK47MsK1lwAG06+5oMv6HySUWxMgoG25BaxK2+0Y77WDUqgaIwnymgOZekBPdPnixP8C4cWqH2gD/HNikVVOEd2gvztsMGbDfAiIkc/ryqcT3SM9ES565qKPsY5ZpDQfXsdl6RMwtwATOg3vLi9OBW4CaaDgIOBwfGppH3Vw== mbc@mbcbook.local\n" +
-      "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAvVnx9RVpN9vPBeLj+vocz7466jUHMI+8CIXjhmbj6Ngh5TyLgh3ahGudnp4yoGoryEwU3f0dy82wMF10pQAtvgJdyRh2jY364hQP72Utbne3NTRH/NA/hg8sg5UQS/lOll52zbOwrTPsODcvsPDzUjbhbvOtowJTLHes/E+aTymQeAFvNj2+rPoIhbW/RxrBs+/b+tQKKOwoC4v1oIx95XKwGA/+IiT5pal6XVmJsMgbDNO3ENu5TzbY99sd5U0pbOIng8JNHZveKMyrtsmtdSnXxPg+w1eZW/c07k+8nbu8RuuCxV2Ax/wWdQfie8Oy3nwf6Ph2CK4vLBMxa/s3YQ== mbc@MacActivo.local\n" +
-      "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAsaBcf6co1Z60Pl57IfoAB20Y1xfWVdw7F2vmKZC83byw1XdQSoUTarkcPOYoFMvAUfTlCGaj4Fn1RTOdvNAIs/p3b6TdZiiEvmIKcpFKoh31Gvul0SbJli279eLm7QxztAz9xTtXbprPAyZdUdkFMQ4TOeJf1nl/Mv8qUvvTQJHn5d+sLW3lPMAbDoPijkSTV9B3H4G0WhHvWmyTNaWVjng68xeEEKFA+d/l8G6AZyFhc7FtryNfpPOV3svahd6kGCDPohphUDAqQX+cJKRmbLJ6VgZhbnAOQQmBoU2QPkp2+0AEmdheIgeuH+B+/ngp0ZlSRmTG1LwZtWAPYqvjSw==",
+                        "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAsaBcf6co1Z60Pl57IfoAB20Y1xfWVdw7F2vmKZC83byw1XdQSoUTarkcPOYoFMvAUfTlCGaj4Fn1RTOdvNAIs/p3b6TdZiiEvmIKcpFKoh31Gvul0SbJli279eLm7QxztAz9xTtXbprPAyZdUdkFMQ4TOeJf1nl/Mv8qUvvTQJHn5d+sLW3lPMAbDoPijkSTV9B3H4G0WhHvWmyTNaWVjng68xeEEKFA+d/l8G6AZyFhc7FtryNfpPOV3svahd6kGCDPohphUDAqQX+cJKRmbLJ6VgZhbnAOQQmBoU2QPkp2+0AEmdheIgeuH+B+/ngp0ZlSRmTG1LwZtWAPYqvjSw== xurde@Macintosh.local",
     :gid => 4001,
     :uid => 4001
   }],
@@ -88,12 +87,12 @@ dna = {
   :yumit => {
     :dir => '/www/yumit',
     :server_name => 'yumit.com',
-    :server_aliases => ['yumit.com'],
+    :server_aliases => ['www.yumit.com', 'beta.yumit.com'],
     :user => 'capistrano',
     :db => {
       :name => 'yumitdb',
       :user => 'root',
-      :password => 'belen3',
+      :password => 'reyes28015',
       :host => 'localhost'
     }
   },
